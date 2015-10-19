d55079393819b0de73cd71c1d1e2d8f242aa2e63
Bendaoud <bendaoud@bendaoud-PC.fr>
2015-10-19 11:40:17 +0200

    MAJ 19/10/2015

--- ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/IServiceCandidature.java
+++ ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/IServiceCandidature.java
@@ -1,8 +1,26 @@
 package eu.telecom_bretagne.cabinet_recrutement.service;
 
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import eu.telecom_bretagne.cabinet_recrutement.data.model.Candidature;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.MessageCand;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.MessageOffre;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.OffreEmploi;
 
 @Remote
 public interface IServiceCandidature {
+	
+	public Candidature addCandidature(Candidature candidature);
+	public List<Candidature> listeCandidature();
+	public Candidature getCandidature(int id);
+	public List<OffreEmploi> offreEmploiByCandidature();
+	public Candidature updateCandidature(Candidature candidature);
+	public void removeCandidature();
+	public MessageCand sendMessageOffre(MessageCand msg);
+	public List<MessageOffre> listeMessageFromEntreprise();
+	public List<MessageCand> listeMessageToEntreprise();
+	
 
 }
--- ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/ServiceCandidature.java
+++ ejbModule/eu/telecom_bretagne/cabinet_recrutement/service/ServiceCandidature.java
@@ -1,7 +1,14 @@
 package eu.telecom_bretagne.cabinet_recrutement.service;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
+import eu.telecom_bretagne.cabinet_recrutement.data.model.Candidature;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.MessageCand;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.MessageOffre;
+import eu.telecom_bretagne.cabinet_recrutement.data.model.OffreEmploi;
 
 /**
  * Session Bean implementation class ServiceCandidature
@@ -16,5 +23,60 @@ public class ServiceCandidature implements IServiceCandidature {
     public ServiceCandidature() {
         // TODO Auto-generated constructor stub
     }
+
+	@Override
+	public Candidature addCandidature(Candidature candidature) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Candidature> listeCandidature() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<OffreEmploi> offreEmploiByCandidature() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Candidature updateCandidature(Candidature candidature) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeCandidature() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public MessageCand sendMessageOffre(MessageCand msg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MessageOffre> listeMessageFromEntreprise() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MessageCand> listeMessageToEntreprise() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Candidature getCandidature(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
